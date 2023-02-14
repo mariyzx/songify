e49f498a69a4e5511867fc510944159ed7c54485
Mariana Werneck <marinhomariana8@gmail.com>
2023-02-14 20:08:41 -0300

    :heavy_plus_sign: :build: add vitest, rtl and jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable import/no-extraneous-dependencies */
+// <reference types="vitest />
+// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
