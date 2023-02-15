88ad7d888732a861246500b59935c854e5bcd526
Mariana Werneck <marinhomariana8@gmail.com>
2023-02-14 21:02:56 -0300

    :sparkles: :feat: add routing on app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
-function App() {
+export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
@@ -11,12 +11,10 @@ function App() {
   );
 }
 
-function WrappedApp() {
+export function WrappedApp() {
   return (
     <HashRouter>
       <App />
     </HashRouter>
   );
 }
-
-export default WrappedApp;
