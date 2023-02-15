0097516a9eab8f197ddefdc180d3fb6f19534efe
Mariana Werneck <marinhomariana8@gmail.com>
2023-02-14 21:16:16 -0300

    :test_tube: :test: add test for Home component

--- tests/Home/Home.test.tsx
+++ tests/Home/Home.test.tsx
@@ -3,12 +3,22 @@ import { render, screen } from '@testing-library/react';
 
 import { WrappedApp } from '../../src/App';
 
-describe('Home', () => {
+describe('Home >', () => {
   it('Renders title', () => {
     // ARRANGE
     render(<WrappedApp />);
     // ACT
     // EXPECT
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent('Home');
+    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
+      'Songify'
+    );
+  });
+
+  it('Renders email and password input', () => {
+    // ARRANGE
+    render(<WrappedApp />);
+    // ACT
+    // EXPECT
+    expect(screen.getByRole('input', { name: 'email' })).toBeInTheDocument();
   });
 });
