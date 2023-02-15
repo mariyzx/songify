9713415cdfdb23652caee0654bbaa0d2974c88bb
Mariana Werneck <marinhomariana8@gmail.com>
2023-02-14 21:36:47 -0300

    :test_tube: :test: add test for button and inputs

--- tests/Home/Home.test.tsx
+++ tests/Home/Home.test.tsx
@@ -8,17 +8,26 @@ describe('Home >', () => {
     // ARRANGE
     render(<WrappedApp />);
     // ACT
+    const title = screen.getByRole('heading', { level: 1 });
     // EXPECT
-    expect(screen.getByRole('heading', { level: 1 })).toHaveTextContent(
-      'Songify'
-    );
+    expect(title).toHaveTextContent('Songify!');
   });
 
   it('Renders email and password input', () => {
     // ARRANGE
     render(<WrappedApp />);
     // ACT
+    const email = screen.getByLabelText('Email:');
+    const pass = screen.getByLabelText('Password:');
     // EXPECT
-    expect(screen.getByRole('input', { name: 'email' })).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+    expect(pass).toBeInTheDocument();
+  });
+
+  it('Renders a login button', () => {
+    render(<WrappedApp />);
+
+    const button = screen.getByRole('button', { name: 'Login' });
+    expect(button).toBeInTheDocument();
   });
 });
