7b9953712cb062b59eb4390b3fb87a21cd540aa4
Mariana <marinhomariana8@gmail.com>
2023-02-23 19:14:37 -0300

    :sparkles: :feat: add interface for loading, albums and getAlbums

--- src/interfaces/IContext.ts
+++ src/interfaces/IContext.ts
@@ -1,6 +1,9 @@
-import { IUser, ICreatedUser } from "./IUser";
+import { IUser, ICreatedUser } from './IUser';
 
 export interface IContext {
-  createUser(data: IUser): ICreatedUser
-  user: ICreatedUser
+  createUser(data: IUser): ICreatedUser;
+  user: ICreatedUser;
+  loading: boolean;
+  albums: Array<object>;
+  getAlbums(artist: string): void;
 }
