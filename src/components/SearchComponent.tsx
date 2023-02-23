2e4ff2c0b85dea53774ea6bee38bd6a83c83a691
Mariana <marinhomariana8@gmail.com>
2023-02-23 19:14:09 -0300

    :sparkles: :feat: add search for albums or artists

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -1,14 +1,38 @@
-const Search = () => {
+import { useContext, useState } from 'react';
+import Context from '../context/Context';
+
+function Search() {
+  const [searchInput, setSearchInput] = useState('');
+  const { loading, getAlbums } = useContext(Context);
+
+  const handleSearch = () => {
+    getAlbums(searchInput);
+    setSearchInput('');
+  };
+
   return (
-    <form>
-      <label htmlFor="search">
-        <input type="text" name="search" id="search" placeholder="Artist or album.." />
-      </label>
-      <button type="button">
-        Search
-      </button>
-    </form>
-  )
+    <div>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <form>
+          <label htmlFor="search">
+            <input
+              type="text"
+              name="search"
+              id="search"
+              value={searchInput}
+              placeholder="Artist or album.."
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </label>
+          <button type="button" onClick={handleSearch}>
+            Search
+          </button>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default Search;
