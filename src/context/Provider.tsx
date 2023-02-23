39134f44be926ce30364783f3c4ad065db1e4b31
Mariana <marinhomariana8@gmail.com>
2023-02-23 19:13:08 -0300

    :sparkles: :feat: add getAlbums function

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,34 +1,47 @@
-import Context from "./Context";
+import { useState } from 'react';
+import Context from './Context';
 import { ICreatedUser, IUser } from '../interfaces/IUser';
-import { useState } from "react";
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
-const Provider = ({ children }: any) => {
+function Provider({ children }: never) {
   const emptyUser = {
     name: '',
     email: '',
     image: '',
-    description: ''
-  }
+    description: '',
+  };
 
   const [user, setUser] = useState<ICreatedUser>(emptyUser);
+  const [loading, setLoading] = useState(false);
+  const [albums, setAlbums] = useState([]);
 
   const createUser = (data: IUser): ICreatedUser => {
-    const info = {...emptyUser, ...data}
-    localStorage.setItem('user', JSON.stringify(info))
-    setUser(info)
+    const info = { ...emptyUser, ...data };
+    localStorage.setItem('user', JSON.stringify(info));
+    setUser(info);
     return info;
-  }
+  };
+
+  const getAlbums = async (artist: string) => {
+    try {
+      setLoading(true);
+      const response = await searchAlbumsAPI(artist);
+      setAlbums(response);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const contextValue = {
     createUser,
-    user
-  }
+    user,
+    getAlbums,
+    albums,
+    loading,
+  };
 
-  return (
-    <Context.Provider value={ contextValue }>
-      { children }
-    </Context.Provider>
-  )
+  return <Context.Provider value={contextValue}>{children}</Context.Provider>;
 }
 
 export default Provider;
