a66dbbdfe9390ee9f0620a792d2c71c26dceddac
Mariana <marinhomariana8@gmail.com>
2023-02-23 18:27:16 -0300

    :recycle: :refactor: add type of user state

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -1,21 +1,27 @@
 import Context from "./Context";
 import { ICreatedUser, IUser } from '../interfaces/IUser';
+import { useState } from "react";
 
 const Provider = ({ children }: any) => {
+  const emptyUser = {
+    name: '',
+    email: '',
+    image: '',
+    description: ''
+  }
+
+  const [user, setUser] = useState<ICreatedUser>(emptyUser);
+
   const createUser = (data: IUser): ICreatedUser => {
-    const emptyUser = {
-      name: '',
-      email: '',
-      image: '',
-      description: ''
-    }
     const info = {...emptyUser, ...data}
     localStorage.setItem('user', JSON.stringify(info))
+    setUser(info)
     return info;
   }
 
   const contextValue = {
-    createUser
+    createUser,
+    user
   }
 
   return (
