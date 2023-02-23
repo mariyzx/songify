89ae1da2863bcfc35743315ba2a540a28aebbd75
Mariana <marinhomariana8@gmail.com>
2023-02-23 18:28:32 -0300

    :sparkles: :feat: add Search page

---
+++ src/pages/Search.tsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import SearchComponent from '../components/SearchComponent';
+
+function Search() {
+  return (
+    <div>
+      <Header />
+      <SearchComponent />
+    </div>
+  );
+}
+
+export default Search;
