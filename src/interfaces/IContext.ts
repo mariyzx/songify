9a9496a9336191ffcf22c9b5ab966dd9f5b52ff7
Mariana <marinhomariana8@gmail.com>
2023-02-23 19:37:32 -0300

    :boom :fix: adjust type of albums

--- src/interfaces/IContext.ts
+++ src/interfaces/IContext.ts
@@ -1,9 +1,11 @@
+import { IAlbum } from './IAlbum';
 import { IUser, ICreatedUser } from './IUser';
 
 export interface IContext {
   createUser(data: IUser): ICreatedUser;
   user: ICreatedUser;
   loading: boolean;
-  albums: Array<object>;
+  albums: Array<IAlbum>;
   getAlbums(artist: string): void;
+  artistName: string;
 }
