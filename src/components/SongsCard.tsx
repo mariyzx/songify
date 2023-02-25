ef4f888a5c1cf44597aea79dec4af4308e43a705
Mariana <marinhomariana8@gmail.com>
2023-02-25 14:15:16 -0300

    :sparkles: :feat: add addFav function

--- src/components/SongsCard.tsx
+++ src/components/SongsCard.tsx
@@ -1,7 +1,21 @@
+import { useContext } from 'react';
+import Context from '../context/Context';
+import { ITarget } from '../interfaces/IHandleTarget';
 import { IProps } from '../interfaces/ISongs';
 
 function SongsCard(props: IProps) {
   const { song } = props;
+  const { addToFav, removeToFav } = useContext(Context);
+
+  const addFav = ({ target }: ITarget) => {
+    const { trackName, previewUrl, artistName, artworkUrl100 } = song;
+    if (target.checked) {
+      addToFav({ trackName, previewUrl, artistName, artworkUrl100 });
+    } else {
+      removeToFav({ trackName, previewUrl, artistName, artworkUrl100 });
+    }
+  };
+
   return (
     <div>
       <h4>{song.trackName}</h4>
@@ -10,7 +24,7 @@ function SongsCard(props: IProps) {
         Your browser doesn&apos;t support the element! <code>audio</code>
       </audio>
       <label htmlFor="favorite">
-        <input type="checkbox" name="favorite" />
+        <input type="checkbox" name="favorite" onChange={(e) => addFav(e)} />
       </label>
     </div>
   );
