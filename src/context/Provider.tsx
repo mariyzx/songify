d589c280d8ac12e508b381eac47b7d12e583c4f2
Mariana <marinhomariana8@gmail.com>
2023-02-25 14:13:32 -0300

    :sparkles: :feat: add addToFav and removeToFav function

--- src/context/Provider.tsx
+++ src/context/Provider.tsx
@@ -3,6 +3,9 @@ import Context from './Context';
 import { ICreatedUser, IUser } from '../interfaces/IUser';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import getMusics from '../services/searchSongsAPI';
+import addToLocal from '../services/favoriteSongsAPI';
+import { ITrack } from '../interfaces/ISongs';
+import { IFavorite } from '../interfaces/IFavorites';
 
 function Provider({ children }: never) {
   const emptyUser = {
@@ -11,16 +14,19 @@ function Provider({ children }: never) {
     image: '',
     description: '',
   };
+  const favs = JSON.parse(localStorage.getItem('favorite_songs')!);
 
   const [user, setUser] = useState<ICreatedUser>(emptyUser);
   const [loading, setLoading] = useState(false);
   const [albums, setAlbums] = useState([]);
   const [artistName, setArtistName] = useState('');
   const [songs, setSongs] = useState([]);
+  const [favSongs, setFavSongs] = useState<IFavorite[]>([]);
 
   const createUser = (data: IUser): ICreatedUser => {
     const info = { ...emptyUser, ...data };
     localStorage.setItem('user', JSON.stringify(info));
+    localStorage.setItem('favorite_songs', JSON.stringify([]));
     setUser(info);
     return info;
   };
@@ -48,6 +54,38 @@ function Provider({ children }: never) {
     }
   };
 
+  const addToFav = (song: ITrack) => {
+    try {
+      setLoading(true);
+      if (favs) {
+        const songExistInFav = favs.filter(
+          (fav: IFavorite) => fav.trackName === song.trackName
+        );
+        if (songExistInFav.length === 0) {
+          const favorites = addToLocal([...favs, song]);
+          setFavSongs(favorites);
+        }
+      }
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const removeToFav = (song: ITrack) => {
+    try {
+      setLoading(true);
+      const newFavs = favs.filter(
+        (fav: IFavorite) => fav.trackName !== song.trackName
+      );
+      addToLocal(newFavs);
+      setFavSongs(newFavs);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const contextValue = {
     createUser,
     user,
@@ -57,6 +95,9 @@ function Provider({ children }: never) {
     artistName,
     getSongs,
     songs,
+    addToFav,
+    removeToFav,
+    favSongs,
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
