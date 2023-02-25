bf7929b201912e351ec8077c57d4a2a9f0cfcd93
Mariana <marinhomariana8@gmail.com>
2023-02-25 13:21:18 -0300

    :sparkles: :feat: rendering details of album

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -1,5 +1,34 @@
+import { useContext, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Context from '../context/Context';
+import SongsCard from './SongsCard';
+
 function Album() {
-  return <div>Album</div>;
+  const { songs, getSongs, artistName, loading } = useContext(Context);
+  const { id } = useParams();
+  const info = songs[0];
+  const tracks = songs.filter((song) => song !== info);
+
+  useEffect(() => {
+    getSongs(id!);
+  }, []);
+
+  return (
+    <div>
+      {songs.length > 0 && (
+        <div>
+          <h2>{info.collectionName}</h2>
+          <img src={info.artworkUrl100} alt={info.collectionName} />
+          <h3>{artistName}</h3>
+        </div>
+      )}
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        tracks.map((song) => <SongsCard song={song} key={song.collectionId} />)
+      )}
+    </div>
+  );
 }
 
 export default Album;
