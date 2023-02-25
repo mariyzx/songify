a706a2e28b074b899a8772a5f7442da2141531aa
Mariana <marinhomariana8@gmail.com>
2023-02-25 13:17:07 -0300

    :sparkles: :feat: add type of tracks

---
+++ src/interfaces/ISongs.ts
@@ -0,0 +1,60 @@
+export interface ISongs {
+  wrapperType: string;
+  collectionType: string;
+  artistId: number;
+  collectionId: number;
+  amgArtistId: number;
+  artistName: string;
+  collectionName: string;
+  collectionCensoredName: string;
+  artistViewUrl: string;
+  collectionViewUrl: string;
+  artworkUrl60: string;
+  artworkUrl100: string;
+  collectionPrice: number;
+  collectionExplicitness: string;
+  trackCount: number;
+  copyright: string;
+  country: string;
+  currency: string;
+  releaseDate: string;
+  primaryGenreName: string;
+}
+
+export interface ITrack {
+  artistId: number;
+  artistName: string;
+  artistViewUrl: string;
+  artworkUrl30: string;
+  artworkUrl60: string;
+  artworkUrl100: string;
+  collectionCensoredName: string;
+  collectionExplicitness: string;
+  collectionId: number;
+  collectionName: string;
+  collectionPrice: number;
+  collectionViewUrl: string;
+  country: string;
+  currency: string;
+  discCount: number;
+  discNumber: number;
+  isStreamable: boolean;
+  kind: string;
+  previewUrl: string;
+  primaryGenreName: string;
+  releaseDate: string;
+  trackCensoredName: string;
+  trackCount: number;
+  trackExplicitness: string;
+  trackId: number;
+  trackName: string;
+  trackNumber: number;
+  trackPrice: number;
+  trackTimeMillis: number;
+  trackViewUrl: string;
+  wrapperType: string;
+}
+
+export interface IProps {
+  song: ITrack;
+}
