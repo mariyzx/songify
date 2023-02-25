8c04b977ceec8b0fac30954c03aa0dffe03f1b7a
Mariana <marinhomariana8@gmail.com>
2023-02-25 13:20:34 -0300

    :recycle: :refactor: removing key

--- src/components/ListAlbums.tsx
+++ src/components/ListAlbums.tsx
@@ -10,7 +10,7 @@ function ListAlbums() {
         <div>
           <h3>Result of albums of: {artistName}</h3>
           {albums.map((album) => (
-            <div key={album.artistId}>
+            <div>
               <img src={album.artworkUrl100} alt={album.collectionName} />
               <h3>{album.collectionName}</h3>
               <p>{album.releaseDate.substring(0, 4)}</p>
