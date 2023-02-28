283d4a9d4a06ea10c54f9fb5826fcd9cbf2e3ae9
Mariana <marinhomariana8@gmail.com>
2023-02-28 16:56:20 -0300

    :sparkles: :feat: add checked state for favorites songs

--- src/components/SongsCard.tsx
+++ src/components/SongsCard.tsx
@@ -1,11 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import Context from '../context/Context';
 import { ITarget } from '../interfaces/IHandleTarget';
 import { IProps } from '../interfaces/ISongs';
+import { IFavorite } from '../interfaces/IFavorites';
 
 function SongsCard(props: IProps) {
   const { song } = props;
   const { addToFav, removeToFav } = useContext(Context);
+  const [fav, setFav] = useState(false);
 
   const addFav = ({ target }: ITarget) => {
     const { trackName, previewUrl, artistName, artworkUrl100 } = song;
@@ -16,6 +18,16 @@ function SongsCard(props: IProps) {
     }
   };
 
+  useEffect(() => {
+    const locals = localStorage.getItem('favorite_songs');
+    if (locals) {
+      const favSong = JSON.parse(locals).find(
+        (local: IFavorite) => local.trackName === song.trackName
+      );
+      setFav(favSong);
+    }
+  }, [fav]);
+
   return (
     <div>
       <h4>{song.trackName}</h4>
@@ -24,7 +36,12 @@ function SongsCard(props: IProps) {
         Your browser doesn&apos;t support the element! <code>audio</code>
       </audio>
       <label htmlFor="favorite">
-        <input type="checkbox" name="favorite" onChange={(e) => addFav(e)} />
+        <input
+          type="checkbox"
+          name="favorite"
+          onChange={(e) => addFav(e)}
+          checked={fav}
+        />
       </label>
     </div>
   );
