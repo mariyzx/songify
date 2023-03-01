35a3c3356c4d60089173f696cf6e4756b6484137
Mariana <marinhomariana8@gmail.com>
2023-03-01 17:14:51 -0300

    :sparkles: :feat: add songify icon on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import Form from '../components/Form';
+import MainHome from '../styles/pages/Home';
+import songify from '../assets/songify.svg';
 
 function Home() {
   return (
-    <div>
+    <MainHome>
+      <img src={songify} alt="songify" />
       <h1>Songify!</h1>
       <Form />
-    </div>
+    </MainHome>
   );
 }
 
