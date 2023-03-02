24b7a2e994ac0294428375129daca310be388e12
Mariana <marinhomariana8@gmail.com>
2023-03-02 18:00:28 -0300

    :recycle: :refactor: add render method with ThemeProvider

--- tests/Home/Home.test.tsx
+++ tests/Home/Home.test.tsx
@@ -1,8 +1,10 @@
 import { describe, it, vi } from 'vitest';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
-import { WrappedApp } from '../../src/App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { App } from '../../src/App';
+import light from '../../src/styles/themes/light';
 
 describe('Home >', () => {
   const setItemSpy = vi.spyOn(Storage.prototype, 'setItem');
@@ -11,71 +13,65 @@ describe('Home >', () => {
     setItemSpy.mockClear();
   });
 
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <ThemeProvider theme={light}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+  });
+
   it('Renders title', () => {
-    // ARRANGE
-    render(<WrappedApp />);
-    // ACT
-    const title = screen.getByRole('heading', { level: 1 });
-    // EXPECT
+    const title = screen.getByRole('heading', { name: /songify!/i });
+
     expect(title).toHaveTextContent('Songify!');
   });
 
   it('Renders email and name input', () => {
-    // ARRANGE
-    render(<WrappedApp />);
-    // ACT
-    const email = screen.getByLabelText('Email:');
-    const name = screen.getByLabelText('Name:');
-    // EXPECT
+    const email = screen.getByLabelText('Email');
+    const name = screen.getByLabelText('Name');
+
     expect(email).toBeInTheDocument();
     expect(name).toBeInTheDocument();
   });
 
   it('Renders a login button', () => {
-    render(<WrappedApp />);
-
     const button = screen.getByRole('button', { name: 'Login' });
+
     expect(button).toBeInTheDocument();
   });
 
   it('Cant login with invalid email', () => {
-    // ARRANGE
-    render(<WrappedApp />);
-    // ACT
-    const email = screen.getByLabelText('Email:');
-    const name = screen.getByLabelText('Name:');
+    const email = screen.getByLabelText('Email');
+    const name = screen.getByLabelText('Name');
     const button = screen.getByRole('button', { name: 'Login' });
     userEvent.type(email, 'invalid@email');
     userEvent.type(name, 'ada');
-    // EXPECT
+
     expect(email).toBeInTheDocument();
     expect(button).toBeDisabled();
   });
 
   it('Cant login with invalid name', () => {
-    // ARRANGE
-    render(<WrappedApp />);
-    // ACT
-    const email = screen.getByLabelText('Email:');
-    const name = screen.getByLabelText('Name:');
+    const email = screen.getByLabelText('Email');
+    const name = screen.getByLabelText('Name');
     const button = screen.getByRole('button', { name: 'Login' });
     userEvent.type(email, 'adalovelace@email.com');
     userEvent.type(name, 'inval');
-    // EXPECT
+
     expect(name).toBeInTheDocument();
     expect(button).toBeDisabled();
   });
 
   it('Email and name are saved on button click', async () => {
-    // ARRANGE
-    render(<WrappedApp />);
-    // ACT
-    const email = screen.getByLabelText('Email:');
-    const name = screen.getByLabelText('Name:');
+    const email = screen.getByLabelText('Email');
+    const name = screen.getByLabelText('Name');
     const button = screen.getByRole('button', { name: 'Login' });
     userEvent.type(email, 'adalovelace@email.com');
     userEvent.type(name, 'Ada');
-    // EXPECT
+
     await waitFor(() => expect(button).not.toBeDisabled());
     userEvent.click(button);
 
