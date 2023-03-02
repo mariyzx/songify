64ce29633f672affad7d440a931314753f8897b3
Mariana <marinhomariana8@gmail.com>
2023-03-02 18:05:02 -0300

    :test_tube: :test: add tests mocks

---
+++ tests/mocks/albums.ts
@@ -0,0 +1,58 @@
+import { IAlbum } from '../../src/interfaces/IAlbum';
+
+const albumMock = [
+  {
+    wrapperType: 'collection',
+    collectionType: 'Album',
+    artistId: 159260351,
+    collectionId: 1468058165,
+    amgArtistId: 816977,
+    artistName: 'Taylor Swift',
+    collectionName: 'Lover',
+    collectionCensoredName: 'Lover',
+    artistViewUrl:
+      'https://music.apple.com/us/artist/taylor-swift/159260351?uo=4',
+    collectionViewUrl: 'https://music.apple.com/us/album/lover/1468058165?uo=4',
+    artworkUrl60:
+      'https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/49/3d/ab/493dab54-f920-9043-6181-80993b8116c9/19UMGIM53909.rgb.jpg/60x60bb.jpg',
+    artworkUrl100:
+      'https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/49/3d/ab/493dab54-f920-9043-6181-80993b8116c9/19UMGIM53909.rgb.jpg/100x100bb.jpg',
+    collectionPrice: 11.99,
+    collectionExplicitness: 'notExplicit',
+    trackCount: 18,
+    copyright: '℗ 2019 Taylor Swift',
+    country: 'USA',
+    currency: 'USD',
+    releaseDate: '2019-08-23T07:00:00Z',
+    primaryGenreName: 'Pop',
+  },
+  {
+    wrapperType: 'collection',
+    collectionType: 'Album',
+    artistId: 159260351,
+    collectionId: 1524801260,
+    amgArtistId: 816977,
+    artistName: 'Taylor Swift',
+    collectionName: 'folklore',
+    collectionCensoredName: 'folklore',
+    artistViewUrl:
+      'https://music.apple.com/us/artist/taylor-swift/159260351?uo=4',
+    collectionViewUrl:
+      'https://music.apple.com/us/album/folklore/1524801260?uo=4',
+    artworkUrl60:
+      'https://is4-ssl.mzstatic.com/image/thumb/Music125/v4/ca/f3/67/caf367a5-2cf6-6b2e-a891-97dc57b19f08/20UMGIM64216.rgb.jpg/60x60bb.jpg',
+    artworkUrl100:
+      'https://is4-ssl.mzstatic.com/image/thumb/Music125/v4/ca/f3/67/caf367a5-2cf6-6b2e-a891-97dc57b19f08/20UMGIM64216.rgb.jpg/100x100bb.jpg',
+    collectionPrice: 12.99,
+    collectionExplicitness: 'explicit',
+    contentAdvisoryRating: 'Explicit',
+    trackCount: 16,
+    copyright: '℗ 2020 Taylor Swift',
+    country: 'USA',
+    currency: 'USD',
+    releaseDate: '2020-07-24T07:00:00Z',
+    primaryGenreName: 'Alternative',
+  },
+];
+
+export default albumMock;
---
+++ tests/mocks/user.ts
@@ -0,0 +1,8 @@
+const mockUser = {
+  name: 'Ada',
+  email: 'adalovelace@email.com',
+  image: '',
+  description: '',
+};
+
+export default mockUser;
