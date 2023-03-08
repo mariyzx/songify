9b3c785f5a559e6ea17509f547fbc9526e85e3bd
Mariana <marinhomariana8@gmail.com>
2023-03-08 03:38:14 -0300

    :lipstick: feat: centering login title on left

---
+++ src/styles/components/Title.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  text-align: left;
+
+  @media only screen and (max-width: 520px) {
+    text-align: center;
+  }
+`;
+
+export default Title;
