ed26016de58b1e17645c508e3f10fa153c7bea9d
Mariana <marinhomariana8@gmail.com>
2023-03-08 03:36:53 -0300

    :lipstick: feat: add padding

--- src/styles/components/Switch.tsx
+++ src/styles/components/Switch.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 
 const Switch = styled.div`
+  display: flex;
   position: fixed;
+  width: 100%;
+  justify-content: space-between;
+  padding-left: 15px;
+  padding-right: 15px;
   top: 5px;
-  right: 5px;
+  align-items: center;
 `;
 
 export default Switch;
