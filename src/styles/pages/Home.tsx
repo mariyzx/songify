b10399e1f21d9f2171878659b74aa533337664be
Mariana <marinhomariana8@gmail.com>
2023-03-08 02:22:33 -0300

    :lipstick: feat: add new media queries

--- src/styles/pages/Home.tsx
+++ src/styles/pages/Home.tsx
@@ -6,28 +6,41 @@ const MainHome = styled.div`
   align-items: center;
   height: 100vh;
   text-align: center;
-  gap: 5rem;
+  gap: 4rem;
+  transition: ease all 0.5s;
 
   img {
     width: 40%;
   }
 
   hr {
-    height: 200px;
-    border-left: 5px solid ${(props) => props.theme.colors.secondary};
-    border-radius: 20%;
+    height: 250px;
+    border: 2px solid ${(props) => props.theme.colors.secondary};
+    border-radius: 5px;
   }
 
   // MOBILE FIRST: 320px - 480px;
   // https://www.emailonacid.com/blog/article/email-development/emailology_media_queries_demystified_min-width_and_max-width/
-  @media only screen and (max-width: 480px) {
+  @media only screen and (max-width: 520px) {
     flex-direction: column;
     height: 100vh;
-    gap: 2rem;
+    gap: 4rem;
 
     img {
       width: 200px;
     }
+
+    hr {
+      display: none;
+    }
+  }
+
+  @media only screen and (min-width: 520px) and (max-width: 600px) {
+    img {
+      width: 200px;
+    }
+
+    gap: 3rem;
   }
 `;
 
