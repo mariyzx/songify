590a987eb22bfb68d68c91d7f3185cb650821708
Mariana <marinhomariana8@gmail.com>
2023-03-09 18:32:35 -0300

    :lipstick: feat: add styles for nav icons

--- src/styles/components/Nav.tsx
+++ src/styles/components/Nav.tsx
@@ -5,9 +5,25 @@ const Nav = styled.nav`
   gap: 1rem;
   flex-direction: column;
   width: 17%;
-  margin-top: 0.5rem;
+  margin-top: 0.7rem;
   height: 100vh;
   padding-left: 15px;
+
+  a:hover {
+    svg {
+      color: ${(props) => props.theme.colors.secondary};
+    }
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    gap: 7px;
+  }
+
+  svg {
+    font-size: 20px;
+  }
 
   @media only screen and (max-width: 630px) {
     flex-direction: row;
