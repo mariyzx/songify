d47735582faaf15bce39bf1e0d3018d2d91526b3
Mariana <marinhomariana8@gmail.com>
2023-03-09 15:27:47 -0300

    :sparkles: feat: add switch on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,33 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from 'styled-components';
+import { shade } from 'polished';
+import ReactSwitch from 'react-switch';
+import Context from '../context/Context';
+import Switch from '../styles/components/Switch';
 
 function Header() {
+  const { toggleTheme } = useContext(Context);
+  const { colors, title } = useContext(ThemeContext);
+
   return (
     <div>
       <header>
         <Link to="/search">
-          <h3>Songify!</h3>
+          <Switch>
+            <h2>Songify</h2>
+            <ReactSwitch
+              onChange={toggleTheme}
+              checked={title === 'dark'}
+              checkedIcon={false}
+              uncheckedIcon={false}
+              height={10}
+              width={40}
+              handleDiameter={20}
+              offColor={shade(0.1, colors.primary)}
+              onColor={colors.secondary}
+            />
+          </Switch>
         </Link>
         <nav>
           <Link to="/search">Search</Link>
