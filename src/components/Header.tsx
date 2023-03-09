3fb65fe288b94893b58fe632d3dec34468f4126c
Mariana <marinhomariana8@gmail.com>
2023-03-09 17:33:51 -0300

    :recycle: :refactor: remove links from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,14 +5,13 @@ import { shade } from 'polished';
 import ReactSwitch from 'react-switch';
 import Context from '../context/Context';
 import Switch from '../styles/components/Switch';
-import Nav from '../styles/components/Nav';
 
 function Header() {
   const { toggleTheme } = useContext(Context);
   const { colors, title } = useContext(ThemeContext);
 
   return (
-    <header>
+    <div>
       <Link to="/search">
         <Switch>
           <h2>Songify</h2>
@@ -29,12 +28,7 @@ function Header() {
           />
         </Switch>
       </Link>
-      <Nav>
-        <Link to="/search">Search</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/profile">Profile</Link>
-      </Nav>
-    </header>
+    </div>
   );
 }
 
