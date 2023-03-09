20ca697b8016fe2cd16d8d1bd7c9311a6910c7ac
Mariana <marinhomariana8@gmail.com>
2023-03-09 18:26:25 -0300

    :lipstick: feat: add responsive style

--- src/styles/components/ListAlbums.tsx
+++ src/styles/components/ListAlbums.tsx
@@ -22,6 +22,7 @@ export const ListAlb = styled.div`
   width: 100%;
   flex-wrap: wrap;
   padding-left: 20px;
+  justify-content: center;
 
   @media only screen and (max-width: 630px) {
     justify-content: center;
@@ -30,26 +31,38 @@ export const ListAlb = styled.div`
 `;
 
 export const CardAlbum = styled.div`
-  width: 250px;
+  width: 230px;
   padding: 10px;
   display: flex;
-  flex-direction: column;
   height: max-content;
   margin: 6px;
   border: 0px solid transparent;
   border-radius: 5px;
+  gap: 4px;
 
   :hover {
     background-color: ${(props) => props.theme.colors.primary};
     transition: all ease-in 0.2s;
+
+    span {
+      display: flex;
+      align-items: flex-end;
+    }
+
     svg {
-      position: absolute;
+      display: flex;
       font-size: 50px;
-      display: block;
       color: ${(props) => props.theme.colors.secondary};
-      margin-left: 17%;
-      margin-top: -5%;
       transition: all ease 0.2s;
+    }
+
+    @media only screen and (max-width: 630px) {
+      flex-direction: column;
+
+      div {
+        justify-content: center;
+        text-align: center;
+      }
     }
   }
 
