a63cebb10db713edc21352082c58d10fa85d56d2
Mariana <marinhomariana8@gmail.com>
2023-03-09 15:46:04 -0300

    :recycle: :refactor: remove unecessary div

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,37 +5,36 @@ import { shade } from 'polished';
 import ReactSwitch from 'react-switch';
 import Context from '../context/Context';
 import Switch from '../styles/components/Switch';
+import Nav from '../styles/components/Nav';
 
 function Header() {
   const { toggleTheme } = useContext(Context);
   const { colors, title } = useContext(ThemeContext);
 
   return (
-    <div>
-      <header>
-        <Link to="/search">
-          <Switch>
-            <h2>Songify</h2>
-            <ReactSwitch
-              onChange={toggleTheme}
-              checked={title === 'dark'}
-              checkedIcon={false}
-              uncheckedIcon={false}
-              height={10}
-              width={40}
-              handleDiameter={20}
-              offColor={shade(0.1, colors.primary)}
-              onColor={colors.secondary}
-            />
-          </Switch>
-        </Link>
-        <nav>
-          <Link to="/search">Search</Link>
-          <Link to="/favorites">Favorites</Link>
-          <Link to="/profile">Profile</Link>
-        </nav>
-      </header>
-    </div>
+    <header>
+      <Link to="/search">
+        <Switch>
+          <h2>Songify</h2>
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={title === 'dark'}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            height={10}
+            width={40}
+            handleDiameter={20}
+            offColor={shade(0.1, colors.primary)}
+            onColor={colors.secondary}
+          />
+        </Switch>
+      </Link>
+      <Nav>
+        <Link to="/search">Search</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/profile">Profile</Link>
+      </Nav>
+    </header>
   );
 }
 
