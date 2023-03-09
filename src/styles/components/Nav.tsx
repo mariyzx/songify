0fc8e989ab5229121c5299afc72a7a80ecdd7f42
Mariana <marinhomariana8@gmail.com>
2023-03-09 17:35:57 -0300

    :lipstick: feat: add styles on Nav component

--- src/styles/components/Nav.tsx
+++ src/styles/components/Nav.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-  margin-top: 2rem;
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  width: 17%;
+  margin-top: 0.5rem;
+  height: 100vh;
+  padding-left: 15px;
 `;
 
 export default Nav;
