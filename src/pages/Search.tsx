aafbe781d5d01c96d6fba57dc1659b213114ec86
Mariana <marinhomariana8@gmail.com>
2023-03-09 17:35:40 -0300

    :sparkles: feat: add nav links

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,26 @@
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import ListAlbums from '../components/ListAlbums';
 import SearchComponent from '../components/SearchComponent';
+import { MainList } from '../styles/components/ListAlbums';
+import Nav from '../styles/components/Nav';
+import { MainSearch } from '../styles/pages/Search';
 
 function Search() {
   return (
     <div>
       <Header />
-      <SearchComponent />
-      <ListAlbums />
+      <MainSearch>
+        <Nav>
+          <Link to="/search">Search</Link>
+          <Link to="/favorites">Favorites</Link>
+          <Link to="/profile">Profile</Link>
+        </Nav>
+        <MainList>
+          <SearchComponent />
+          <ListAlbums />
+        </MainList>
+      </MainSearch>
     </div>
   );
 }
