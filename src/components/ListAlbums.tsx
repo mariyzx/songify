e2498505bb99ed692260404c680fdea8f1513881
Mariana <marinhomariana8@gmail.com>
2023-03-09 17:34:26 -0300

    :lipstick: feat: add styles on ListAlbums

--- src/components/ListAlbums.tsx
+++ src/components/ListAlbums.tsx
@@ -1,27 +1,34 @@
 import { useContext } from 'react';
-import { Link } from 'react-router-dom';
 import Context from '../context/Context';
+import { DivAlbum, CardAlbum, ListAlb } from '../styles/components/ListAlbums';
 
 function ListAlbums() {
   const { albums, artistName, empty } = useContext(Context);
   return (
-    <div>
+    <DivAlbum>
       {!empty ? (
-        <div>
+        <>
           <h3>Result of albums of: {artistName}</h3>
-          {albums.map((album) => (
-            <div key={album.artworkUrl100}>
-              <img src={album.artworkUrl100} alt={album.collectionName} />
-              <h3>{album.collectionName}</h3>
-              <p>{album.releaseDate.substring(0, 4)}</p>
-              <Link to={`/album/${album.collectionId}`}>Details</Link>
-            </div>
-          ))}
-        </div>
+          <ListAlb>
+            {albums.map((album) => (
+              <div key={album.artworkUrl100}>
+                <CardAlbum to={`/album/${album.collectionId}`}>
+                  <img src={album.artworkUrl100} alt={album.collectionName} />
+                  <h3>{album.collectionName}</h3>
+                  <div>
+                    <p>{album.releaseDate.substring(0, 4)}</p>
+                    <span>•</span>
+                    <p>{artistName}</p>
+                  </div>
+                </CardAlbum>
+              </div>
+            ))}
+          </ListAlb>
+        </>
       ) : (
         <h3>Sorry, not found! :(</h3>
       )}
-    </div>
+    </DivAlbum>
   );
 }
 
