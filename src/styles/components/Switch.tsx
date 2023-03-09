8a82ae2f6755a688f5a143c995747a47d944085c
Mariana <marinhomariana8@gmail.com>
2023-03-09 15:42:21 -0300

    :recycle: :refactor: remove position fixed

--- src/styles/components/Switch.tsx
+++ src/styles/components/Switch.tsx
@@ -2,12 +2,11 @@ import styled from 'styled-components';
 
 const Switch = styled.div`
   display: flex;
-  position: fixed;
   width: 100%;
   justify-content: space-between;
   padding-left: 15px;
   padding-right: 15px;
-  top: 5px;
+  padding-top: 5px;
   align-items: center;
 `;
 
