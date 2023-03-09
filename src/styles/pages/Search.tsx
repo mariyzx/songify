4c85552a85ee4fee3f12f17a61bfdd58bef95e20
Mariana <marinhomariana8@gmail.com>
2023-03-09 17:37:36 -0300

    :lipstick: feat: add styles on Search page

---
+++ src/styles/pages/Search.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const MainSearch = styled.div`
+  height: 94vh;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const SearchForm = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
