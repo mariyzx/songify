3fc096a655b9861f40310575560d0d612c87e2e9
Mariana <marinhomariana8@gmail.com>
2023-03-13 12:08:33 -0300

    :lipstick: feat: add styles for profile

---
+++ src/styles/pages/Profile.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const MainProfile = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  a {
+    text-decoration: underline;
+  }
+`;
+
+export default MainProfile;
