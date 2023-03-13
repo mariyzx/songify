f8bba813938a6baf837b824ed45319c17b11b7be
Mariana <marinhomariana8@gmail.com>
2023-03-13 12:08:41 -0300

    :lipstick: feat: add styles for profileEdit

---
+++ src/styles/pages/ProfileEdit.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Edit = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1rem;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1rem;
+  }
+`;
+
+export default Edit;
