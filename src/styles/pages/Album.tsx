02f49b7c87f58e93e142653c4359513766267dcf
Mariana <marinhomariana8@gmail.com>
2023-03-13 11:26:14 -0300

    :lipstick: feat: add styles for divAlbum

--- src/styles/pages/Album.tsx
+++ src/styles/pages/Album.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const MainAlbum = styled.div`
+export const DivAlbum = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -9,4 +9,8 @@ const MainAlbum = styled.div`
   padding: 1rem;
 `;
 
-export default MainAlbum;
+export const MainAlbum = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
