8add2422c5924843d0b9378706d229cb2885e947
Mariana <marinhomariana8@gmail.com>
2023-03-13 09:51:30 -0300

    :lipstick: feat: add style for Album page

---
+++ src/styles/pages/Album.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const MainAlbum = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+  margin-top: 10px;
+`;
+
+export default MainAlbum;
