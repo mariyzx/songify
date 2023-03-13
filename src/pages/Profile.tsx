fb7249efd0555dd9bd9f64198d3a1e068f352957
Mariana <marinhomariana8@gmail.com>
2023-03-13 12:07:29 -0300

    :lipstick: feat: add styles for profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import { ICreatedUser } from '../interfaces/IUser';
+import MainProfile from '../styles/pages/Profile';
 
 function Profile() {
   const emptyUser = {
@@ -21,12 +22,14 @@ function Profile() {
   return (
     <div>
       <Header />
-      <h1>Profile</h1>
-      <img src={user.image} alt={user.name} />
-      <h2>Name: {user.name}</h2>
-      <h3>Email: {user.email}</h3>
-      <h4>Description: {user.description}</h4>
-      <Link to="/profile/edit">Edit</Link>
+      <MainProfile>
+        <h1>Profile</h1>
+        <img src={user.image} alt={user.name} />
+        <h2>Name: {user.name}</h2>
+        <h3>Email: {user.email}</h3>
+        <h4>Description: {user.description}</h4>
+        <Link to="/profile/edit">Edit</Link>
+      </MainProfile>
     </div>
   );
 }
