de3c6c4af397a40285df92dfb546154cf23e0d29
Mariana <marinhomariana8@gmail.com>
2023-03-13 10:56:42 -0300

    :lipstick: feat: add styles for songs list

---
+++ src/styles/components/SongsCard.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Songs = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+  gap: 1rem;
+`;
+
+export const DivSongs = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 6px;
+`;
