57a5f4da804395acf55fd7c551751de41de3a108
Mariana <marinhomariana8@gmail.com>
2023-03-14 19:42:19 -0300

    :sparkles: feat: add icons for light and dark mode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import ReactSwitch from 'react-switch';
 import { ThemeContext } from 'styled-components';
-import { shade } from 'polished';
+import { lighten, shade } from 'polished';
+import { BsSun, BsMoon } from 'react-icons/bs';
 import Form from '../components/Form';
 import MainHome from '../styles/pages/Home';
 import songify from '../assets/songify.svg';
 import Context from '../context/Context';
-import Switch from '../styles/components/Switch';
+import { DivSwitch, Switch } from '../styles/components/Switch';
 import Title from '../styles/components/Title';
 import Footer from '../styles/components/Footer';
 
@@ -18,17 +19,22 @@ function Home() {
     <div>
       <Switch>
         <h2>Songify</h2>
-        <ReactSwitch
-          onChange={toggleTheme}
-          checked={title === 'dark'}
-          checkedIcon={false}
-          uncheckedIcon={false}
-          height={10}
-          width={40}
-          handleDiameter={20}
-          offColor={shade(0.1, colors.primary)}
-          onColor={colors.secondary}
-        />
+        <DivSwitch>
+          <BsSun />
+          <ReactSwitch
+            onChange={toggleTheme}
+            checked={title === 'dark'}
+            checkedIcon={false}
+            uncheckedIcon={false}
+            height={10}
+            width={40}
+            boxShadow={`2px 2px 18px ${lighten(0.1, colors.secondary)}`}
+            handleDiameter={20}
+            offColor={shade(0.1, colors.primary)}
+            onColor={colors.secondary}
+          />
+          <BsMoon />
+        </DivSwitch>
       </Switch>
       <MainHome>
         <img src={songify} alt="songify" />
