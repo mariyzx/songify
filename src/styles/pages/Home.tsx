289b2fe545efb6dacc7731e75f75cff935dd5b37
Mariana <marinhomariana8@gmail.com>
2023-03-15 19:10:52 -0300

    :lipstick: feat: add bigger height for hr

--- src/styles/pages/Home.tsx
+++ src/styles/pages/Home.tsx
@@ -10,18 +10,18 @@ export const MainContent = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  gap: 2rem;
+  gap: 6rem;
   height: 95vh;
   transition: ease all 0.5s;
 
   hr {
-    height: 250px;
+    height: 320px;
     border: 2px solid ${(props) => props.theme.colors.secondary};
     border-radius: 5px;
   }
 
   img {
-    width: 230px;
+    width: 280px;
   }
 
   // MOBILE FIRST: 320px - 480px;
@@ -52,7 +52,7 @@ export const TitleDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 3.5rem;
+  gap: 1.5rem;
 
   @media only screen and (max-width: 600px) {
     gap: 1rem;
