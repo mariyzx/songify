2ebd718a50dc98e839a968d22a72c33ad7c2bad4
Mariana <marinhomariana8@gmail.com>
2023-03-29 18:46:19 -0300

    :boom: fix: adjust of param type

--- backend/src/repositories/Register.repository.ts
+++ backend/src/repositories/Register.repository.ts
@@ -1,15 +1,23 @@
-import { IRegisterUser, IRegisterResponse } from '../interfaces/IRegister';
+import { IRegisterResponse, IRegisterCredentials } from '../interfaces/IRegister';
 import { IRegisterRepository } from '../interfaces/repositories/IRegisterRepository';
 import Prisma from '../model';
 
 export class RegisterRepository implements IRegisterRepository {
-	async register(data: IRegisterUser): Promise<IRegisterResponse | null> {
+	async register(data: IRegisterResponse): Promise<IRegisterResponse | null> {
 		return (await Prisma.user.create({
 			data: {
 				email: data.email,
 				password: data.password,
 				name: data.name,
 				token: data.token,
+			}
+		}));
+	}
+
+	async findUser(data: IRegisterCredentials): Promise<IRegisterResponse | null> {
+		return (await Prisma.user.findUnique({
+			where: {
+				email: data.email
 			}
 		}));
 	}
