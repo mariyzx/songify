a4da3606d63c909d9f971c363470df2bcebd65ee
Mariana <marinhomariana8@gmail.com>
2023-03-30 15:23:26 -0300

    :sparkles: feat: add validateRegister ou route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { CreateRegisterControllerFactory } from '../factories/RegisterControllerFactory';
+import validateRegister from '../utils/validators';
 
 const router = Router();
 const registerController = CreateRegisterControllerFactory.make();
 
-router.post('/register', (req, res) => registerController.register(req, res));
+router.post('/register', validateRegister, (req, res) => registerController.register(req, res));
 
 export default router;
