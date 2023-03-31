4d41f1d74ef65297c72b96534a0af98194a5b99d
Mariana <marinhomariana8@gmail.com>
2023-03-31 13:54:43 -0300

    :boom: fix: adjust next function

--- backend/src/middlewares/validators.ts
+++ backend/src/middlewares/validators.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
-const validateRegister = async (req: Request, res: Response) => {
+const validateRegister = async (req: Request, res: Response, next: NextFunction) => {
 	const schema = Joi.object({
 		email:  Joi.string().email().required(),
 		name: Joi.string().min(1).required(),
@@ -10,6 +10,8 @@ const validateRegister = async (req: Request, res: Response) => {
   
 	const { error } = schema.validate(req.body);
 	if (error) res.status(400).json({message: error.details[0].message});
+
+	next();
 };
 
 export default validateRegister;
