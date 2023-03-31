678cd7297eec8f8fe58a415c679cc1e9149f505a
Mariana <marinhomariana8@gmail.com>
2023-03-31 14:57:31 -0300

    :boom: fix: add optional favorite songs

--- backend/src/interfaces/ILogin.ts
+++ backend/src/interfaces/ILogin.ts
@@ -1,4 +1,13 @@
+import { IFavoriteSongs } from './IFavoriteSongs';
+
 export interface ILoginCredentials {
   email: string;
   password: string;
+}
+
+export interface ILoginResponse {
+  name: string;
+  email: string;
+  token: string;
+  favoriteSongs?: IFavoriteSongs[] 
 }
