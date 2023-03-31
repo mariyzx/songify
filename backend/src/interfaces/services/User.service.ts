b8bdc506c814689c0a3d0b40cf71fad58ac2fd5a
Mariana <marinhomariana8@gmail.com>
2023-03-31 14:57:58 -0300

    :sparkles: feat: add login function

--- backend/src/interfaces/services/User.service.ts
+++ backend/src/interfaces/services/User.service.ts
@@ -1,5 +1,7 @@
-import { IRegisterCredentials, IRegisterResponse } from '../IRegister';
+import { ILoginCredentials, ILoginResponse } from '../ILogin';
+import { IRegisterCredentials, IRegisterUser } from '../IRegister';
 
 export interface IUserService {
-  register(user: IRegisterCredentials): Promise<IRegisterResponse | null> 
+  register(user: IRegisterCredentials): Promise<IRegisterUser | null> 
+  login(user: ILoginCredentials): Promise<ILoginResponse | null>
 }
