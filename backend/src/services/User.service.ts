a6b599fee5b4551bd9ed2778ae8d4a08aa171d27
Mariana <marinhomariana8@gmail.com>
2023-03-31 14:24:29 -0300

    :boom: fix: remove token generate

--- backend/src/services/User.service.ts
+++ backend/src/services/User.service.ts
@@ -1,25 +1,21 @@
 import md5 from 'md5';
-import { IRegisterCredentials, IRegisterResponse } from '../interfaces/IRegister';
+import { IRegisterCredentials, IRegisterUser } from '../interfaces/IRegister';
 import { IUserRepository } from '../interfaces/repositories/UserRepository';
-import { jwtGen } from '../utils/jwt';
 
 export default class RegisterService {
 	constructor(
     readonly userRepository: IUserRepository
 	){}
 
-	async register(user: IRegisterCredentials): Promise<IRegisterResponse | null> {
+	async register(user: IRegisterCredentials): Promise<IRegisterUser | null> {
 		// verifica se o usuário já existe
 		const userExists = await this.userRepository.findUser(user);
 		//  return { status: 409, message: 'User already registered' };
 		if (userExists) return null;
 
-		const { email } = user;
-
 		const password = md5(user.password);
-		const token = jwtGen({email});
     
-		const createdUser = await this.userRepository.register({ ...user, token, password});
+		const createdUser = await this.userRepository.register({ ...user, password});
 		return createdUser;
 	}
 }
