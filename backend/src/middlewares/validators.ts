ef79bb580deb481f1ba927be95c0d19f00df502c
Mariana <marinhomariana8@gmail.com>
2023-03-31 15:25:12 -0300

    :sparkles: feat: add login validator

--- backend/src/middlewares/validators.ts
+++ backend/src/middlewares/validators.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import Joi from 'joi';
 
-const validateRegister = async (req: Request, res: Response, next: NextFunction) => {
+export const validateRegister = async (req: Request, res: Response, next: NextFunction) => {
 	const schema = Joi.object({
 		email:  Joi.string().email().required(),
 		name: Joi.string().min(1).required(),
@@ -14,4 +14,15 @@ const validateRegister = async (req: Request, res: Response, next: NextFunction)
 	next();
 };
 
-export default validateRegister;
+export const validateLogin = async (req: Request, res: Response, next: NextFunction) => {
+	const schema = Joi.object({
+		email:  Joi.string().email().required(),
+		password: Joi.string().min(4).required(),
+	});
+  
+	const { error } = schema.validate(req.body);
+	if (error) res.status(400).json({message: error.details[0].message});
+
+	next();
+};
+
