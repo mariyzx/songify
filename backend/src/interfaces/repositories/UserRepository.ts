6b35e884f804b72f9ff23386e2bda419cb2f9267
Mariana <marinhomariana8@gmail.com>
2023-03-31 14:23:18 -0300

    :boom: fix: add correct types

--- backend/src/interfaces/repositories/UserRepository.ts
+++ backend/src/interfaces/repositories/UserRepository.ts
@@ -1,6 +1,7 @@
-import { IRegisterCredentials, IRegisterResponse } from '../IRegister';
+import { ILoginCredentials } from '../ILogin';
+import { IRegisterCredentials, IRegisterUser } from '../IRegister';
 
 export interface IUserRepository {
-  register(data: IRegisterResponse): Promise<IRegisterResponse | null>
-  findUser(data: IRegisterCredentials): Promise<IRegisterResponse | null>
+  register(data: IRegisterCredentials): Promise<IRegisterUser | null>
+  findUser(data: ILoginCredentials): Promise<IRegisterUser | null>
 }
