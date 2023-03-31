eedbb15d25c7793fac2327a313d20925cd7a15ab
Mariana <marinhomariana8@gmail.com>
2023-03-31 15:25:33 -0300

    :sparkles: feat: add validateLogin middleware

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { CreateUserControllerFactory } from '../factories/UserControllerFactory';
-import validateRegister from '../middlewares/validators';
+import { validateRegister, validateLogin } from '../middlewares/validators';
 
 const router = Router();
 const userController = CreateUserControllerFactory.make();
 
 router.post('/register', validateRegister, (req, res) => userController.register(req, res));
-router.post('/login', (req, res) => userController.login(req, res));
+router.post('/login', validateLogin, (req, res) => userController.login(req, res));
 
 export default router;
