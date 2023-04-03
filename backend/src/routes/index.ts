2feb20fb2ce081c59922fbf5fadef401b33762ca
Mariana <marinhomariana8@gmail.com>
2023-04-03 16:45:05 -0300

    :sparkles: feat: add authMiddleware on /favorite endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { CreateUserControllerFactory } from '../factories/UserControllerFactory';
-import { validateRegister, validateLogin, validateFavSongs } from '../middlewares/validators';
+import { validateRegister, validateLogin, validateFavSongs, authMiddleware } from '../middlewares/validators';
 
 const router = Router();
 const userController = CreateUserControllerFactory.make();
 
 router.post('/register', validateRegister, (req, res) => userController.register(req, res));
 router.post('/login', validateLogin, (req, res) => userController.login(req, res));
-router.patch('/favorite', validateFavSongs, (req, res) => userController.addToFav(req, res));
+router.patch('/favorite', validateFavSongs, authMiddleware, (req, res) => userController.addToFav(req, res));
 
 export default router;
