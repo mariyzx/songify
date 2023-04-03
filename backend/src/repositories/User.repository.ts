36753df3e4afa1b221d8689bc2bd398db8f317c4
Mariana <marinhomariana8@gmail.com>
2023-04-03 15:15:34 -0300

    :sparkles: feat: add createSong and addToFav function

--- backend/src/repositories/User.repository.ts
+++ backend/src/repositories/User.repository.ts
@@ -1,3 +1,4 @@
+import { IFavoriteSongs, IFavoriteSongsParam } from '../interfaces/IFavoriteSongs';
 import { ILoginCredentials } from '../interfaces/ILogin';
 import { IRegisterCredentials, IRegisterUser } from '../interfaces/IRegister';
 import { IUserRepository } from '../interfaces/repositories/UserRepository';
@@ -20,5 +21,56 @@ export class UserRepository implements IUserRepository {
 				email: data.email
 			}
 		}));
+	}
+
+	async createSongs(songs: IFavoriteSongs[]): Promise<IFavoriteSongs[]> {
+		const createdSongs = await Promise.all(songs.map(song =>
+			Prisma.songs.create({
+				data: {
+					title: song.title,
+					artist: song.artist,
+					album: song.album
+				}
+			})
+		));
+	
+		return createdSongs;
+	}
+
+	async addToFav(data: IFavoriteSongsParam) {
+		const { user: { email }, songs } = data;
+		const songIds = songs.map(song => song.id);
+
+		const existingSongs = await Prisma.songs.findMany({
+			where: {
+				id: {
+					in: songIds
+				}
+			}
+		});
+
+		// Verifica se todos os registros foram encontrados
+		if (existingSongs.length !== songIds.length) {
+			// Cria os registros que não existem
+			const missingSongIds = songIds.filter(id => !existingSongs.find(song => song.id === id));
+			const newSongs = await this.createSongs(songs.filter(song => missingSongIds.includes(song.id)));
+		
+			// Adiciona os novos registros à lista de registros existentes
+			existingSongs.push(...newSongs);
+		}
+
+
+		const updatedUser = await Prisma.user.update({
+			where: {
+				email
+			},
+			data: {
+				favoriteSongs: {
+					connect: existingSongs.map(song => ({ id: song.id })),
+				}
+			}
+		});
+
+		return updatedUser;
 	}
 }
