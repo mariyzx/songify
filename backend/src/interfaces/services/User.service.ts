cabfc55794206340f7feb54ffd5056a49f8cfef5
Mariana <marinhomariana8@gmail.com>
2023-04-03 15:52:11 -0300

    :sparkles: feat: add data type on addToFav function on service

--- backend/src/interfaces/services/User.service.ts
+++ backend/src/interfaces/services/User.service.ts
@@ -1,9 +1,9 @@
-import { IFavoriteSongsParam } from '../IFavoriteSongs';
+import { IFavoriteSongsParam, IFavoriteSongsResponseWithoutPass } from '../IFavoriteSongs';
 import { ILoginCredentials, ILoginResponse } from '../ILogin';
 import { IRegisterCredentials, IRegisterUser } from '../IRegister';
 
 export interface IUserService {
   register(user: IRegisterCredentials): Promise<IRegisterUser | null> 
   login(user: ILoginCredentials): Promise<ILoginResponse | null>
-  addToFav(data: IFavoriteSongsParam): void
+  addToFav(data: IFavoriteSongsParam): Promise<IFavoriteSongsResponseWithoutPass | null>
 }
