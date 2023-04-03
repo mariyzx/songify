098f5a311bcd824570a643c1a0a4d60cae5131dc
Mariana <marinhomariana8@gmail.com>
2023-04-03 19:14:28 -0300

    :sparkles: feat: add handleRegister function

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,17 +1,57 @@
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useContext, useEffect, useState } from 'react';
 import { Button, Label } from '../styles/components/Form';
 import Header from '../components/Header';
 import MainSignUp from '../styles/pages/SignUp';
+import Context from '../context/Context';
+
+const schema = z.object({
+  name: z.string().min(1),
+  email: z.string().email().min(3),
+  password: z.string().min(6),
+});
+
+type SchemaType = z.infer<typeof schema>;
 
 function SignUp() {
+  const {
+    register,
+    formState: { isDirty, isValid },
+    getValues,
+    reset,
+  } = useForm<SchemaType>({
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+  const { createUser, statusCode } = useContext(Context);
+
+  const handleRegister = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    createUser(getValues());
+    reset({ name: '', email: '', password: '' });
+  };
+
   return (
     <div>
       <Header />
       <MainSignUp>
         <h2>Create your account!</h2>
         <form>
-          <Label htmlFor="email">
+          <Label htmlFor="name">
             <h4>Name</h4>
-            <input type="name" id="name" placeholder="Ada Lovelace" />
+            <input
+              type="name"
+              id="name"
+              placeholder="Ada Lovelace"
+              {...register('name')}
+            />
           </Label>
           <Label htmlFor="email">
             <h4>Email</h4>
@@ -19,14 +59,27 @@ function SignUp() {
               type="email"
               id="email"
               placeholder="adalovelace@email.com"
+              {...register('email')}
             />
           </Label>
           <Label htmlFor="password">
             <h4>Password</h4>
-            <input type="text" id="password" placeholder="******" />
+            <input
+              type="password"
+              id="password"
+              placeholder="******"
+              {...register('password')}
+            />
           </Label>
-          <Button type="submit">Sign Up</Button>
+          <Button
+            type="submit"
+            onClick={(e) => handleRegister(e)}
+            disabled={!isDirty || !isValid}
+          >
+            Sign Up
+          </Button>
         </form>
+        {statusCode !== 201 ? <p>Error while trying to sign up!</p> : null}
       </MainSignUp>
     </div>
   );
