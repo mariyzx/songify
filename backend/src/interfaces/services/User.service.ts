2d916f5223c92c53c60df3b04b9051b78a33c66a
Mariana <marinhomariana8@gmail.com>
2023-04-03 15:15:11 -0300

    :sparkles: feat: add addToFav function

--- backend/src/interfaces/services/User.service.ts
+++ backend/src/interfaces/services/User.service.ts
@@ -1,7 +1,9 @@
+import { IFavoriteSongsParam } from '../IFavoriteSongs';
 import { ILoginCredentials, ILoginResponse } from '../ILogin';
 import { IRegisterCredentials, IRegisterUser } from '../IRegister';
 
 export interface IUserService {
   register(user: IRegisterCredentials): Promise<IRegisterUser | null> 
   login(user: ILoginCredentials): Promise<ILoginResponse | null>
+  addToFav(data: IFavoriteSongsParam): void
 }
