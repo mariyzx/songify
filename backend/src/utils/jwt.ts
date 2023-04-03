728c6e83efc2321d4c82add2a2fd81964cf7ec0e
Mariana <marinhomariana8@gmail.com>
2023-04-03 16:44:29 -0300

    :sparkles: feat: add jwtVerify function

--- backend/src/utils/jwt.ts
+++ backend/src/utils/jwt.ts
@@ -4,4 +4,10 @@ const SECRET = process.env.JWT_SECRET || 'my_secret';
 
 const jwtGen = (data: object) => jwt.sign({...data}, SECRET, { expiresIn: '1d'});
 
+export const jwtVerify = (token: string) => {
+	const decoded = jwt.verify(token, SECRET);
+
+	return decoded;
+};
+
 export { jwtGen };
