9cf2c7d27f6af7e474e3df357daef1c8c5f00dfd
Mariana <marinhomariana8@gmail.com>
2023-04-03 18:03:36 -0300

    :sparkles: feat: add header on signup pae

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { Button, Label } from '../styles/components/Form';
+import Header from '../components/Header';
+import MainSignUp from '../styles/pages/SignUp';
 
-const SignUp = () => {
+function SignUp() {
   return (
     <div>
-      Cadastrar
+      <Header />
+      <MainSignUp>
+        <h2>Create your account!</h2>
+        <form>
+          <Label htmlFor="email">
+            <h4>Name</h4>
+            <input type="name" id="name" placeholder="Ada Lovelace" />
+          </Label>
+          <Label htmlFor="email">
+            <h4>Email</h4>
+            <input
+              type="email"
+              id="email"
+              placeholder="adalovelace@email.com"
+            />
+          </Label>
+          <Label htmlFor="password">
+            <h4>Password</h4>
+            <input type="text" id="password" placeholder="******" />
+          </Label>
+          <Button type="submit">Sign Up</Button>
+        </form>
+      </MainSignUp>
     </div>
-  )
+  );
 }
 
 export default SignUp;
