55ce3e9a32191cee61a87a5cffd196a1b278881c
Mariana <marinhomariana8@gmail.com>
2023-04-07 15:15:21 -0300

    :sparkles: feat: add interface for getSongs

--- backend/src/interfaces/services/User.service.ts
+++ backend/src/interfaces/services/User.service.ts
@@ -1,4 +1,4 @@
-import { IFavoriteSongsParam, IFavoriteSongsResponseWithoutPass } from '../IFavoriteSongs';
+import { IFavoriteSongs, IFavoriteSongsParam, IFavoriteSongsResponseWithoutPass } from '../IFavoriteSongs';
 import { ILoginCredentials, ILoginResponse } from '../ILogin';
 import { IRegisterCredentials, IRegisterUser } from '../IRegister';
 
@@ -6,4 +6,5 @@ export interface IUserService {
   register(user: IRegisterCredentials): Promise<IRegisterUser | null> 
   login(user: ILoginCredentials): Promise<ILoginResponse | null>
   addToFav(data: IFavoriteSongsParam): Promise<IFavoriteSongsResponseWithoutPass | null>
+  getSongs(email: string): Promise<IFavoriteSongs[] | null>
 }
