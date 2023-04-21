79db637aa821c938de4476f3bf1229f4a4fbc2df
Mariana <marinhomariana8@gmail.com>
2023-04-21 16:02:35 -0300

    :sparkles: feat: add delete /favorite route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -9,5 +9,7 @@ router.post('/register', validateRegister, (req, res) => userController.register
 router.post('/login', validateLogin, (req, res) => userController.login(req, res));
 router.patch('/favorite', validateFavSongs, authMiddleware, (req, res) => userController.addToFav(req, res));
 router.get('/favorite', (req, res) => userController.getSongs(req, res));
+router.delete('/favorite', validateFavSongs, authMiddleware,
+	(req, res) => userController.removeSong(req, res));
 
 export default router;
