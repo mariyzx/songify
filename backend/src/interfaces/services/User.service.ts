6a162419e7ee16afc33ef70b90356f51c3972b10
Mariana <marinhomariana8@gmail.com>
2023-04-27 14:29:08 -0300

    :sparkles: feat: add updateUser service interface

--- backend/src/interfaces/services/User.service.ts
+++ backend/src/interfaces/services/User.service.ts
@@ -1,5 +1,5 @@
 import { IFavoriteSongs, IFavoriteSongsParam, IFavoriteSongsResponseWithoutPass } from '../IFavoriteSongs';
-import { ILoginCredentials, ILoginResponse } from '../ILogin';
+import { ILoginCredentials, ILoginResponse, ILoginUpdate, ILoginUpdateResponse } from '../ILogin';
 import { IRegisterCredentials, IRegisterUser } from '../IRegister';
 
 export interface IUserService {
@@ -8,4 +8,5 @@ export interface IUserService {
   addToFav(data: IFavoriteSongsParam): Promise<IFavoriteSongsResponseWithoutPass | null>
   getSongs(email: string): Promise<IFavoriteSongs[] | null>
   removeToFav(data: IFavoriteSongsParam): Promise<IFavoriteSongsResponseWithoutPass | null>
+  updateUser(data: ILoginUpdate): Promise<ILoginUpdateResponse | null>
 }
