a2d60ee197d28b360a675748d92d545ba807edb8
Mariana <marinhomariana8@gmail.com>
2023-05-02 13:27:44 -0300

    :sparkles: feat: add /docs endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import { CreateUserControllerFactory } from '../factories/UserControllerFactory';
 import { validateRegister, validateLogin, validateFavSongs, authMiddleware, validateUpdate } from '../middlewares/validators';
+import * as doc from '../swagger.json';
+import * as swaggerUI from 'swagger-ui-express';
 
 const router = Router();
 const userController = CreateUserControllerFactory.make();
 
+router.use('/docs', swaggerUI.serve, swaggerUI.setup(doc));
 router.post('/register', validateRegister, (req, res) => userController.register(req, res));
 router.post('/login', validateLogin, (req, res) => userController.login(req, res));
 router.patch('/favorite', validateFavSongs, authMiddleware, (req, res) => userController.addToFav(req, res));
