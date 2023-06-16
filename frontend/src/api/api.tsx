cc39d6e68db433589f9b6fff54053666843f4fa3
Mariana Werneck <marinhomariana8@gmail.com>
2023-06-16 15:41:47 -0300

    :boom: fix: add https on request

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -2,5 +2,5 @@ import react from 'react';
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://songify-production.up.railway.app/', // trocar pela url do deploy
+  baseURL: 'https://songify-production.up.railway.app/', // trocar pela url do deploy
 });
