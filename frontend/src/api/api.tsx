c0086a9129d65fa757875c2c6521aeb9949f2bb9
Mariana Werneck <marinhomariana8@gmail.com>
2023-08-29 12:48:24 -0300

    fix: add backend port

--- frontend/src/api/api.tsx
+++ frontend/src/api/api.tsx
@@ -2,5 +2,5 @@ import react from 'react';
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://songify-production.up.railway.app/', // trocar pela url do deploy
+  baseURL: 'http://localhost:3003/', // trocar pela sua porta do backend
 });
