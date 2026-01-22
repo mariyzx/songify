747770d1d13aa923b14b7644c68276c4220a8a06
Mariana Werneck <marinhomariana8@gmail.com>
2026-01-21 22:12:27 -0300

    feat: add JWT authentication strategy, guard, and current user decorator

---
+++ api-songify/src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: payload.email,
+      },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        description: true,
+        favoriteSongs: true,
+      },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid token');
+    }
+    return user;
+  }
+}
